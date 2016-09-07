24bcc4e5c200a9fe90c121d9528cdf80ea7bc691
Aysegul Yonet <aysegul.yonet@autodesk.com>
2016-09-07 12:45:27 -0700

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { routing } from './core.routes';
+import { CoreComponent } from './core.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    routing
+  ],
+  declarations: [
+    CoreComponent
+  ]
+})
+export class CoreModule { }
